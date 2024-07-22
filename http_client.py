9808b42833d312f3eb936c3c2eb2f4a3b74bb56a
andriidelz <161312069+andriidelz@users.noreply.github.com>
2024-07-22 20:37:35 +0300

    Add files via upload

--- http_client.py
+++ http_client.py
@@ -1,4 +1,5 @@
 from aiohttp import ClientSession
+from async_lru import alru_cache
 
 
 class HTTPClient:
@@ -6,21 +7,23 @@ class HTTPClient:
         self._session = ClientSession(
             base_url=base_url,
             headers={
-                'X-CMC_PRO_API_KEY': api_key,
+                'X-CMC_PRO_API_KEY': api_key
             }
         )
 
 
 class CMCHTTPClient(HTTPClient):
+    @alru_cache
     async def get_listings(self):
         async with self._session.get("/v1/cryptocurrency/listings/latest") as resp:
             result = await resp.json()
             return result["data"]
 
+    @alru_cache
     async def get_currency(self, currency_id: int):
         async with self._session.get(
             url="/v2/cryptocurrency/quotes/latest",
             params={"id": currency_id}
         ) as resp:
             result = await resp.json()
-            return result["data"]
+            return result["data"][str(currency_id)]
